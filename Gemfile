d2c5b43e290f9c6779590cf776fdc1401a7054fa
Dave Tapley <dukedave@gmail.com>
2012-03-15 22:30:53 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+require 'rbconfig'
+HOST_OS = RbConfig::CONFIG['host_os']
+
+source :rubygems, :github
+
+gem 'rake',  '>=0.9.2'
+gem 'rails', '3.2.2'
+
+gem 'mysql'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanzman::Application.config.secret_token = '9e6bbd70d35813e65f60ef44d6df6a2a5f6a466169eff458a09ba4827d47a793e15dba1b1dac07f8af78b3ade59a2dfbb00ab8f43a9143e195a1601790822789'
